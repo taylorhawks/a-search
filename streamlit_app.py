3075453ebba4f6f85b61e0229f9bc47f1f30c9a8
Taylor Hawks <taylor.a.hawks@gmail.com>
2024-09-19 11:14:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,21 @@ import streamlit as st
 st.title('A* Search')
 st.header('Taylor Hawks')
 
-st.selectbox(
-  label = 'terrain', 
-  options = ['⛰️'], 
-  index=0, 
-  # format_func=special_internal_function, 
-  # key=None, 
-  # help=None, 
-  # on_change=None, 
-  # args=None, 
-  # kwargs=None, *, 
-  placeholder="Choose an option", 
-  disabled=False, 
-  label_visibility="visible"
-)
+WIDTH = 20
+HEIGHT = 20
+
+for x in range(WIDTH):
+  st.selectbox(
+    label = 'terrain', 
+    options = ['⛰️'], 
+    index=0, 
+    # format_func=special_internal_function, 
+    # key=None, 
+    # help=None, 
+    # on_change=None, 
+    # args=None, 
+    # kwargs=None, *, 
+    placeholder="Choose an option", 
+    disabled=False, 
+    label_visibility="visible"
+  )
